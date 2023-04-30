cbac0c22d30f5446b510acc2df8f22d7b49a8a2d
jumpincrane <michalnt98@gmail.com>
2023-04-30 16:00:43 +0200

    updated dice loss

--- src/DiagnosisAI/custom_loss.py
+++ src/DiagnosisAI/custom_loss.py
@@ -1,19 +1,27 @@
 import torch.nn as nn
 import torch
 
+
 class DiceLoss(nn.Module):
+    """This definition generalize to real valued pred and target vector. This should be differentiable.
+    :param float smooth,
+
+    In forward docstring:
+    :param torch.Tensor inputs: tensor with first dimension as batch,
+    :param torch.Tensor targets: tensor with first dimension as batch.
+    """
+
     def __init__(self, smooth: float = 1.0):
         super(DiceLoss, self).__init__()
-
         self.smooth = smooth
 
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
-             
-        inputs = nn.functional.softmax(inputs, dim=1)
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice = (2.*intersection + self.smooth)/(inputs.sum() + targets.sum() + self.smooth)  
-        
-        return 1 - dice
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        # have to use contiguous since they may from a torch.view op
+        iflat = inputs.contiguous().view(-1)
+        tflat = targets.contiguous().view(-1)
+        intersection = (iflat * tflat).sum()
+
+        A_sum = torch.sum(tflat * iflat)
+        B_sum = torch.sum(tflat * tflat)
+
+        return 1 - ((2. * intersection + self.smooth) / (A_sum + B_sum + self.smooth))
